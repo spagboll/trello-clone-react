c6618f77936319c7e7977eb74bfc13ccee11b02c
Zach Hodges <zach.hodges@ao.com>
2021-05-09 23:48:39 +0100

    Working on edit button for name of card

--- src/Cards.js
+++ src/Cards.js
@@ -5,6 +5,7 @@ import style from "./css/Cards.module.css";
 const Cards = () => {
   const [tasks, setTasks] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [editable, setEditable] = useState(false);
 
   useEffect(() => {
     const doGetTasks = async () => {
@@ -31,21 +32,33 @@ const Cards = () => {
     );
   };
 
+  const handleEditOnClick = () => {
+    setEditable(true);
+  };
+
   return (
     <ul className={style.card.ul}>
       {tasks.map(({ name, id, isCompleted }) => (
         <li key={id} className={style.cardDefault}>
-          {name}{" "}
-          <input
-            type="checkbox"
-            onChange={() => handleCheckboxChange(id)}
-            checked={isCompleted}
-            disabled={isLoading}
-          />
+          {name}
+          {
+            <button hidden={editable} onClick={handleEditOnClick}>
+              Edit
+            </button>
+          }
+          {<button hidden={!editable}>Save</button>}
         </li>
       ))}
     </ul>
   );
 };
+
+/* <input
+            type="checkbox"
+            onChange={() => handleCheckboxChange(id)}
+            checked={isCompleted}
+            disabled={isLoading}
+            
+          /> */
 
 export default Cards;
